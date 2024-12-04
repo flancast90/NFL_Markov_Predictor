38a7fc8b471bc2ccc2c72736bf8e29c6004062cc
callie <88816725+QBCPotate@users.noreply.github.com>
2024-12-04 13:03:29 -0500

    Update to CLI

--- cli.py
+++ cli.py
@@ -18,10 +18,10 @@ def main():
         "--validate", action="store_true", help="Validate the trained model"
     )
     parser.add_argument(
-        "--dataset", help="Select the desired dataset"
+        "--dataset", help="Select the desired dataset (Default dataset is data/nfl_dataset.json)"
     )
     parser.add_argument(
-        "--model", help="Select the name of the model (Will save at saves/model_*model*.pkl)"
+        "--model", help="Select the name of the model (Will save at model/saves/model_*model*.json, defaults to model/saves/trained_model.json)"
     )
 
 
@@ -31,6 +31,7 @@ def main():
         print("Dataset located at " + args.dataset + " loaded")
 
     if args.process:
+        """Process the given data model into a training set and validation set"""
         processor = ModelProcessor()
         if args.dataset is not None:
             processor.process_and_save(args.dataset)
@@ -39,18 +40,23 @@ def main():
         print("Processed data and saved train/validation sets")
 
     if args.train:
+        """Train a new data model using the training set"""
         trainer = ModelTrainer()
         trainer.load_training_data()
         trainer.train()
         if args.model is not None:
-            trainer.save_model(args.model)
+            trainer.save_model("model/saves/model_" + args.model + ".json")
         else:
             trainer.save_model()
         print("Trained and saved model")
 
     if args.validate:
+        """Validate the selected data model using validation set"""
         validator = ModelValidator()
-        validator.load_model()
+        if args.model is not None:
+            validator.load_model("model/saves/model_" + args.model + ".json")
+        else:
+            validator.load_model()
         validator.load_validation_data()
         accuracy, confusion_matrix, total_profit, roi = validator.validate()
         print(f"Model validation complete. Accuracy: {accuracy:.4f}")
